4ccb7979bea5eb0d30c8709a577eaa2c2188eee4
alanam79 <halo2382001@gmail.com>
2022-05-13 12:22:26 -0400

    finished up styling and initial set up for page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,64 @@
 var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
+var nameInputEl = document.querySelector("#ingredientname");
+var chuckJokesButtonEl = document.getElementById("joke-form");
+var chuckJokesEl = document.getElementById("chuck-jokes");
+
 
 var formSubmitHandler = function (event) {
   // prevent page from refreshing
   event.preventDefault();
   // get value from input element
-  var state = nameInputEl.value.trim();
+  var ingredientName = nameInputEl.value.trim();
 
-  if (state) {
-    // renderRecents();
+  if (ingredientName) {
+    getIngredientCard(ingredientName)
     // clear old content
     nameInputEl.value = "";
-	console.log(state);
+	console.log(ingredientName);
   } else {
-    alert("Please enter a State!");
+    alert("");
   }
 };
 
-// alchol api
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com',
-		'X-RapidAPI-Key': '90378b7530msh7d82c41a463e11ep1da2d8jsne90cccb035d1'
-	}
+var jokeSubmitHandler = function (event) {
+  event.preventDefault();
+  getChuckJokes();
+  // console.log(event); <-- verify click event happened
 };
 
-fetch('https://the-cocktail-db.p.rapidapi.com/search.php?i=vodka', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+var displayChuckJokes = function(chuckJokes) {
+  console.log(chuckJokes);
+};
+
+
+// var getIngredientCard = function (ingredientName) {
+// // alchol api
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com',
+// 		'X-RapidAPI-Key': '90378b7530msh7d82c41a463e11ep1da2d8jsne90cccb035d1'
+// 	}
+// };
+
+// fetch('https://the-cocktail-db.p.rapidapi.com/search.php?i=vodka' + ingredientName)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+// }
+
+var getChuckJokes = function() {
+
+  var apiURL = "https://api.chucknorris.io/jokes/random";
+
+  fetch(apiURL).then(function(response) {
+    response.json().then(function(data) {
+      displayChuckJokes(data);
+    });
+  });
+};
+
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+chuckJokesButtonEl.addEventListener("click", jokeSubmitHandler);
