792686cb9d33c9e2b978a49f575c92d60ec45ba9
dan <danielornelas01@yahoo.com>
2022-05-16 18:01:19 -0700

    fixer

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,16 +25,18 @@ var formSubmitHandler = function (event) {
 
   console.log("newName in saveName", nameEntered);
 
-  name.push(nameEntered);
+  console.log(username)
+  if (username) {
+    name.push(nameEntered);
   console.log("name entered", name);
   localStorage.setItem("name", JSON.stringify(name));
-
-  if (username) {
-    getNameAge(username);
+getNameAge(username);
     // clear old content
     nameInputEl.value = "";
     return username;
   } else {
+    var modal = document.getElementById("simpleModal");
+    console.log(modal)
     // listen for click
     openModal();
     //modalBtn.addEventListener("click", openModal);
@@ -60,7 +62,7 @@ var formSubmitHandler = function (event) {
     }
 
     // get modal element
-    var modal = document.getElementById("simpleModal");
+    
     // get open modal button
   }
 };
